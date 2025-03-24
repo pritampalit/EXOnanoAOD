59b3b901ef57001f2ee5e6740a5808ac3d259f50
PRITAM PALIT <pritampalit@gmail.com>
2025-03-24 04:41:27 +0100

    Module for JetImpactParameters Added

--- python/custom_displacedtau_cff.py
+++ python/custom_displacedtau_cff.py
@@ -10,6 +10,109 @@ from PhysicsTools.NanoAOD.custom_btv_cff import *
 from Configuration.Eras.Modifier_run3_common_cff import run3_common
 import os
 
+'''
+def addPFCandsDxyz(process, allPF = False, addAK4=False, addAK8=False):
+    process.customizedPFCandsTask = cms.Task()
+    process.schedule.associate(process.customizedPFCandsTask)
+    
+    process.finalJetsAK8Constituents = cms.EDProducer("PatJetConstituentPtrSelector",
+                                                      src = cms.InputTag("finalJetsAK8"),
+                                                      cut = cms.string("")
+                                                      )
+    process.finalJetsAK4Constituents = cms.EDProducer("PatJetConstituentPtrSelector",
+                                                      src = cms.InputTag("finalJetsPuppi"),
+                                                      cut = cms.string("")
+                                                      )
+    if allPF:
+        candInput = cms.InputTag("packedPFCandidates")
+    elif not addAK8:
+        candList = cms.VInputTag(cms.InputTag("finalJetsAK4Constituents", "constituents"))
+        process.customizedPFCandsTask.add(process.finalJetsAK4Constituents)
+        process.finalJetsConstituentsTable = cms.EDProducer("PackedCandidatePtrMerger", src = candList, skipNulls = cms.bool(True), warnOnSkip = cms.bool(True))
+        candInput = cms.InputTag("finalJetsConstituentsTable")
+    elif not addAK4:
+        candList = cms.VInputTag(cms.InputTag("finalJetsAK8Constituents", "constituents"))
+        process.customizedPFCandsTask.add(process.finalJetsAK8Constituents)
+        process.finalJetsConstituentsTable = cms.EDProducer("PackedCandidatePtrMerger", src = candList, skipNulls = cms.bool(True), warnOnSkip = cms.bool(True))
+        candInput = cms.InputTag("finalJetsConstituentsTable")
+    else:
+        candList = cms.VInputTag(cms.InputTag("finalJetsAK4Constituents", "constituents"), cms.InputTag("finalJetsAK8Constituents", "constituents"))
+        process.customizedPFCandsTask.add(process.finalJetsAK4Constituents)
+        process.customizedPFCandsTask.add(process.finalJetsAK8Constituents)
+        process.finalJetsConstituentsTable = cms.EDProducer("PackedCandidatePtrMerger", src = candList, skipNulls = cms.bool(True), warnOnSkip = cms.bool(True))
+        candInput = cms.InputTag("finalJetsConstituentsTable")
+        
+    process.customConstituentsExtTable = cms.EDProducer("SimplePATCandidateFlatTableProducer",
+                                                        src = candInput,
+                                                        cut = cms.string(""), #we should not filter after pruning
+                                                        name = cms.string("PFCands"),
+                                                        doc = cms.string("interesting particles from AK4 and AK8 jets"),
+                                                        singleton = cms.bool(False), # the number of entries is variable
+                                                        extension = cms.bool(False), # this is the extension table for the AK8 constituents
+                                                        variables = cms.PSet(##//CandVars,
+                                                            dz = Var("?hasTrackDetails()?dz():-1", float, doc="pf dz", precision=10),
+                                                            dzErr = Var("?hasTrackDetails()?dzError():-1", float, doc="pf dz err", precision=10),
+                                                            d0 = Var("?hasTrackDetails()?dxy():-1", float, doc="pf d0", precision=10),
+                                                            d0Err = Var("?hasTrackDetails()?dxyError():-1", float, doc="pf d0 err", precision=10),
+                                                            trkP = Var("?hasTrackDetails()?pseudoTrack().p():-1", float, doc="track momemtum", precision=-1),
+                                                            trkPt = Var("?hasTrackDetails()?pseudoTrack().pt():-1", float, doc="track pt", precision=-1),
+                                                            trkEta = Var("?hasTrackDetails()?pseudoTrack().eta():-1", float, doc="track pt", precision=12),
+                                                            trkPhi = Var("?hasTrackDetails()?pseudoTrack().phi():-1", float, doc="track phi", precision=12),
+                                                            ),
+                                                        externalVariables = cms.PSet(
+                                                            rankByPt = cms.VPSet(
+                                                                cms.PSet(
+                                                                    tag = cms.string("rank"),
+                                                                    quantity = cms.string("-pt()")  # Sorting by descending pt
+                                                                )
+                                                            )
+                                                        ),
+                                                        
+                                                        maxLen = cms.uint32(50) 
+                                                        
+
+                                                        )
+    kwargs = { }
+    import os
+    sv_sort = os.getenv('CMSSW_NANOAOD_SV_SORT')
+    if sv_sort is not None: kwargs['sv_sort'] = cms.untracked.string(sv_sort)
+    pf_sort = os.getenv('CMSSW_NANOAOD_PF_SORT')
+    if pf_sort is not None: kwargs['pf_sort'] = cms.untracked.string(pf_sort)
+    process.customAK8ConstituentsTable = cms.EDProducer("PatJetConstituentTableProducer",
+                                                        candidates = candInput,
+                                                        jets = cms.InputTag("finalJetsAK8"),
+                                                        jet_radius = cms.double(0.8),
+                                                        name = cms.string("FatJetPFCands"),
+                                                        idx_name = cms.string("pFCandsIdx"),
+                                                        nameSV = cms.string("FatJetSVs"),
+                                                        idx_nameSV = cms.string("sVIdx"),
+                                                        **kwargs,
+                                                        )
+    process.customAK4ConstituentsTable = cms.EDProducer("PatJetConstituentTableProducer",
+                                                        candidates = candInput,
+                                                        jets = cms.InputTag("finalJetsPuppi"), # was finalJets before
+                                                        jet_radius = cms.double(0.4),
+                                                        name = cms.string("JetPFCands"),
+                                                        idx_name = cms.string("pFCandsIdx"),
+                                                        nameSV = cms.string("JetSVs"),
+                                                        idx_nameSV = cms.string("sVIdx"),
+                                                        **kwargs,
+                                                        )
+    process.customizedPFCandsTask.add(process.customConstituentsExtTable)
+
+    if not allPF:
+        process.customizedPFCandsTask.add(process.finalJetsConstituentsTable)
+    # linkedObjects are WIP for Run3
+    if addAK8:
+        process.customizedPFCandsTask.add(process.customAK8ConstituentsTable)
+    if addAK4: 
+        process.customizedPFCandsTask.add(process.customAK4ConstituentsTable)
+    
+        
+    return process
+'''
+
+
 def add_displacedtauTables(process, isMC, useCHSJets = True):
 
     if isMC:
@@ -31,39 +134,67 @@ def add_displacedtauTables(process, isMC, useCHSJets = True):
         file_string = "/afs/cern.ch/work/p/ppalit2/public/tau_pog_reco/exonanoaod_v2/CMSSW_15_0_0_pre3/src/PhysicsTools/EXOnanoAOD/data/particlenet_v1_a27159734e304ea4b7f9e0042baa9e22.pb"
 
     process.disTauTag = cms.EDProducer(
-          "DisTauTag",
+            "DisTauTag",
         graphPath = cms.string(file_string),
         jets = process.jetTable.src,
         pfCandidates = cms.InputTag('packedPFCandidates'),
         save_inputs  = cms.bool(False)
     )
     
-    d_disTauTagVars = {
-          "disTauTag_score0":     ExtVar("disTauTag:score0"       , float, doc = "Score 0"),
-          "disTauTag_score1":     ExtVar("disTauTag:score1"       , float, doc = "Score 1"),
-    }
-
-    # Create the task                                                                                                                             
-    print ('adding disTau edproducer')
-    if useCHSJets:
-      process.jetTable.externalVariables = process.jetTable.externalVariables.clone(**d_disTauTagVars)
-    ## for puppi jets, use this!                                                                                                                  
-    else:
-      process.jetPuppiTable.externalVariables = process.jetPuppiTable.externalVariables.clone(**d_disTauTagVars)
-
-    ##//process.custom_nanoaod_task = cms.Task(  
-    process.exonanoaodTask = cms.Task(
-        process.disTauTag,
+    process.jetImpactParameters = cms.EDProducer(
+        "JetImpactParameters",
+        jets = process.jetTable.src,
+        pfCandidates = cms.InputTag('packedPFCandidates'),
+        deltaRMax = cms.double(0.4)
     )
 
-    ##//process.schedule.associate(process.custom_nanoaod_task)
-    process.nanoTableTaskCommon.add(process.exonanoaodTask)
+    
+    
+    d_disTauTagVars = {
+        "disTauTag_score0":     ExtVar("disTauTag:score0"       , float, doc = "Score 0"),
+        "disTauTag_score1":     ExtVar("disTauTag:score1"       , float, doc = "Score 1"),
+        "dxy": ExtVar("jetImpactParameters:jetDxy", float, doc = "leadingPtPFCand_dxy which is within dR=0.4 and charged/hasTrackDetails"),
+        "dz": ExtVar("jetImpactParameters:jetDz", float, doc = "leadingPtPFCand_dz which is within dR=0.4 and charged/hasTrackDetails"),
+        "dxyerror": ExtVar("jetImpactParameters:jetDxyError", float, doc = "leadingPtPFCand_dxyerror which is within dR=0.4 and charged/hasTrackDetails"),
+        "dzerror": ExtVar("jetImpactParameters:jetDzError", float, doc = "leadingPtPFCand_dzerror which is within dR=0.4 and charged/hasTrackDetails"), 
+    }
+
+    print ('adding disTau edproducer')
+    if useCHSJets:
+        process.jetTable.externalVariables = process.jetTable.externalVariables.clone(**d_disTauTagVars)
+        ## for puppi jets, use this!
+        
+    else:
+        process.jetPuppiTable.externalVariables = process.jetPuppiTable.externalVariables.clone(**d_disTauTagVars)
+      
+    ##//process.custom_nanoaod_task = cms.Task(  
+    process.disTauTagTask = cms.Task(process.disTauTag)
+    process.jetImpactParametersTask = cms.Task(process.jetImpactParameters)
+    ##process.schedule.associate(process.custom_nanoaod_task)
+    ##//process.schedule.associate(process.exonanoaodTask1, process.exonanoaodTask2)
+    ##//process.nanoTableTaskCommon.add(process.exonanoaodTask)
+    ##//process.nanoTableTaskCommon.add(process.exonanoaodTask1)
+    process.nanoTableTaskCommon.add(process.disTauTagTask, process.jetImpactParametersTask)
     
     return process
 
 
 def BTVCustomNanoAODStaus(process, useCHSJets = True):
     addPFCands(process,btvNano_switch.btvNano_addallPF_switch,btvNano_switch.btvNano_addAK4_switch,btvNano_switch.btvNano_addAK8_switch)
+    
+    ### for MC   
+    process.load("PhysicsTools.NanoAOD.btvMC_cff")
+    process.nanoSequenceMC+=ak4onlyPFCandsMCSequence
+
+    if useCHSJets:
+        process.finalJetsAK4Constituents.src = src = cms.InputTag("finalJets")
+        process.customAK4ConstituentsTable.jets = cms.InputTag("finalJets")
+    
+    return process
+
+
+def BTVCustomNanoAODStausDxyzInfo(process, useCHSJets = True):
+    addPFCandsDxyz(process,btvNano_switch.btvNano_addallPF_switch,btvNano_switch.btvNano_addAK4_switch,btvNano_switch.btvNano_addAK8_switch)
     
     ### for MC   
     process.load("PhysicsTools.NanoAOD.btvMC_cff")
