206e1c68647d281786fde235059d7e83cf6f633f
PRITAM PALIT <pritampalit@gmail.com>
2025-06-02 13:55:41 +0200

    Try for adding CHS collection with Puppi collection

--- test/Run3_2023_PAT_EXONANO_template.py
+++ test/Run3_2023_PAT_EXONANO_template.py
@@ -8,6 +8,7 @@ from Configuration.Eras.Era_Run3_cff import Run3
 
 process = cms.Process('NANO',Run3)
 
+
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
@@ -19,6 +20,7 @@ process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('PhysicsTools.PatAlgos.slimming.metFilterPaths_cff')
 process.load('Configuration.StandardSequences.PATMC_cff')
 process.load('PhysicsTools.NanoAOD.nano_cff')
+process.load('PhysicsTools.NanoAOD.nano_chsjet_cff')
 process.load('PhysicsTools.NanoAOD.btvMC_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
@@ -30,8 +32,8 @@ process.maxEvents = cms.untracked.PSet(
 
 # Input source
 process.source = cms.Source("PoolSource",
-                            ##fileNames = cms.untracked.vstring('/store/group/lpcdisptau/Staus_M_100_100mm_13p6TeV_Run3Summer22/AODSIM/231007_160225/0000/Run3Summer22AOD-LLStau_M100_ctau100mm_1.root'),
-    fileNames = cms.untracked.vstring('/store/mc/Run3Summer23DRPremix/TTto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8/AODSIM/130X_mcRun3_2023_realistic_v14-v2/70000/0073f4e8-97f3-4564-8fc3-e33bbb8842ee.root'),
+                        fileNames = cms.untracked.vstring('/store/group/lpcdisptau/Staus_M_100_100mm_13p6TeV_Run3Summer22/AODSIM/231007_160225/0000/Run3Summer22AOD-LLStau_M100_ctau100mm_1.root'),
+    #fileNames = cms.untracked.vstring('/store/mc/Run3Summer23DRPremix/TTto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8/AODSIM/130X_mcRun3_2023_realistic_v14-v2/70000/0073f4e8-97f3-4564-8fc3-e33bbb8842ee.root'),
     secondaryFileNames = cms.untracked.vstring()
 )
 
@@ -76,6 +78,8 @@ process.configurationMetadata = cms.untracked.PSet(
 
 # Output definition
 
+#from PhysicsTools.EXOnanoAOD.custom_displacedtau_cff import *
+
 process.NANOAODoutput = cms.OutputModule("NanoAODOutputModule",
     compressionAlgorithm = cms.untracked.string('LZMA'),
     compressionLevel = cms.untracked.int32(9),
@@ -83,10 +87,22 @@ process.NANOAODoutput = cms.OutputModule("NanoAODOutputModule",
         dataTier = cms.untracked.string('NANOAOD'),
         filterName = cms.untracked.string('')
     ),
-    fileName = cms.untracked.string('Run3_2023_PAT_EXONANO_template_woPFCand_TT.root'),
+    fileName = cms.untracked.string('Run3_2023_PAT_EXONANO_template_test_TT_v2.root'),
     outputCommands = process.NANOAODEventContent.outputCommands
 
                                          )
+
+'''
+process.NANOAODoutput.outputCommands.extend([
+    'keep nanoaodFlatTable_JeCHSt*_*_*',
+    ##'keep nanoaodFlatTable_Jet*_*_*'
+    
+])
+'''
+
+
+
+
 # Additional output definition
 
 # Other statements
@@ -124,12 +140,13 @@ process.Flag_trkPOG_manystripclus53X = cms.Path(~process.manystripclus53X)
 process.Flag_trkPOG_toomanystripclus53X = cms.Path(~process.toomanystripclus53X)
 
 process.nanoAOD_step = cms.Path(process.nanoSequenceMC)
+process.nanoAOD_step_CHS = cms.Path(process.nanoSequenceCommonCHS)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.NANOAODoutput_step = cms.EndPath(process.NANOAODoutput)
 
 # Schedule definition
 
-process.schedule = cms.Schedule(process.Flag_HBHENoiseFilter,process.Flag_HBHENoiseIsoFilter,process.Flag_CSCTightHaloFilter,process.Flag_CSCTightHaloTrkMuUnvetoFilter,process.Flag_CSCTightHalo2015Filter,process.Flag_globalTightHalo2016Filter,process.Flag_globalSuperTightHalo2016Filter,process.Flag_HcalStripHaloFilter,process.Flag_hcalLaserEventFilter,process.Flag_EcalDeadCellTriggerPrimitiveFilter,process.Flag_EcalDeadCellBoundaryEnergyFilter,process.Flag_ecalBadCalibFilter,process.Flag_goodVertices,process.Flag_eeBadScFilter,process.Flag_ecalLaserCorrFilter,process.Flag_trkPOGFilters,process.Flag_chargedHadronTrackResolutionFilter,process.Flag_muonBadTrackFilter,process.Flag_BadChargedCandidateFilter,process.Flag_BadPFMuonFilter,process.Flag_BadPFMuonDzFilter,process.Flag_hfNoisyHitsFilter,process.Flag_BadChargedCandidateSummer16Filter,process.Flag_BadPFMuonSummer16Filter,process.Flag_trkPOG_manystripclus53X,process.Flag_trkPOG_toomanystripclus53X,process.Flag_trkPOG_logErrorTooManyClusters,process.nanoAOD_step,process.endjob_step,process.NANOAODoutput_step)
+process.schedule = cms.Schedule(process.Flag_HBHENoiseFilter,process.Flag_HBHENoiseIsoFilter,process.Flag_CSCTightHaloFilter,process.Flag_CSCTightHaloTrkMuUnvetoFilter,process.Flag_CSCTightHalo2015Filter,process.Flag_globalTightHalo2016Filter,process.Flag_globalSuperTightHalo2016Filter,process.Flag_HcalStripHaloFilter,process.Flag_hcalLaserEventFilter,process.Flag_EcalDeadCellTriggerPrimitiveFilter,process.Flag_EcalDeadCellBoundaryEnergyFilter,process.Flag_ecalBadCalibFilter,process.Flag_goodVertices,process.Flag_eeBadScFilter,process.Flag_ecalLaserCorrFilter,process.Flag_trkPOGFilters,process.Flag_chargedHadronTrackResolutionFilter,process.Flag_muonBadTrackFilter,process.Flag_BadChargedCandidateFilter,process.Flag_BadPFMuonFilter,process.Flag_BadPFMuonDzFilter,process.Flag_hfNoisyHitsFilter,process.Flag_BadChargedCandidateSummer16Filter,process.Flag_BadPFMuonSummer16Filter,process.Flag_trkPOG_manystripclus53X,process.Flag_trkPOG_toomanystripclus53X,process.Flag_trkPOG_logErrorTooManyClusters,process.nanoAOD_step,process.nanoAOD_step_CHS,process.endjob_step,process.NANOAODoutput_step)
 
 process.schedule.associate(process.patTask)
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
@@ -141,7 +158,13 @@ associatePatAlgosToolsTask(process)
 
 #process = BTVCustomNanoAODStaus(process, useCHSJets = use_CHS_jets) # to keep PF Candidates of AK4 jets
 
-from PhysicsTools.NanoAOD.nano_cff import nanoAOD_customizeCommon
+#use_CHS_jets = True
+#from PhysicsTools.EXOnanoAOD.custom_displacedtau_cff import *
+#run3modifier_chs(process, useCHSJets = use_CHS_jets)    
+
+#process = BTVCustomNanoAODStausDxyzInfo(process, useCHSJets = use_CHS_jets) # to keep PF Candidates of AK4 jets
+
+from PhysicsTools.NanoAOD.nano_cff import nanoAOD_customizeCommon, nanoTableTaskCommon
 
 process = nanoAOD_customizeCommon(process)
 
@@ -167,6 +190,12 @@ from PhysicsTools.EXOnanoAOD.custom_displacedtau_cff import *
 process = add_displacedtauCHSTables(process, 1)
 
 # End of customisation functions
+
+print("--- DEBUG: Final Output Commands ---")
+
+print("process.NANOAODoutput.outputCommands : ", process.NANOAODoutput.outputCommands)
+
+print("------------------------------------")
 
 # Customisation from command line
 
